ff05b550b2528bf3f53469c8bc7e4d9e3c5d353f
filix <florindozec@gmail.com>
2025-05-25 11:40:48 +0000

    fix carrateri e spazi lista tags

--- back_end/middleware/Middlewares.js
+++ back_end/middleware/Middlewares.js
@@ -1,7 +1,7 @@
 export let queryParamsToList = (listParams = []) => (req, res, next) => {
 
     listParams.forEach((value) => {
-        if (req.query[value] !== undefined) req[value] = req.query[value].split(',');
+        if (req.query[value] !== undefined) req[value] = req.query[value].split(',').map(item => item.toLowerCase().trim());
     })
     next();
 
