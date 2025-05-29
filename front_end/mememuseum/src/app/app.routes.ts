20cc0ec85604b45b3da691d8cdb3e0a3a5cc89ce
filix <florindozec@gmail.com>
2025-05-29 18:30:56 +0000

    crezione home page e auth

--- front_end/mememuseum/src/app/app.routes.ts
+++ front_end/mememuseum/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Homepage } from './homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        title: "Home Page",
+        component: Homepage
+    }
+
+
+];
