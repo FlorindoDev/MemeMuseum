5bc39daf89f7f80e94fb0bc12a86b4e0dfe00349
filix <florindozec@gmail.com>
2025-05-31 15:34:25 +0000

    aggiunta dei colori dinamici e inizio navber

--- front_end/mememuseum/tailwind.config.js
+++ front_end/mememuseum/tailwind.config.js
@@ -5,8 +5,17 @@ module.exports = {
         "./src/**/*.{html,ts}",
         "./src/index.{html,ts}"
     ],
+
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                text: 'var(--color-text)',
+                background: 'var(--color-bg)',
+                primary: 'var(--color-primary)',
+                secondary: 'var(--color-secondary)',
+                accent: 'var(--color-accent)',
+            },
+        },
     },
     plugins: [],
 }
